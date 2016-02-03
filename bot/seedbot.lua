3041a59a1d97d26164d8847d67be44aad99210cf
ashkanpj <ashkanpj.ap33@gmail.com>
2016-02-03 20:48:34 +0330

    u

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,29 +226,20 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {81444630},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[Telefire v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@ashi_admin1_kaboos [Founder]
+@sajad_admin2_kaboos [Developer]
+
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+Ashi sudo and sajad admin
 ]],
     help_text_realm = [[
 Realm Commands:
