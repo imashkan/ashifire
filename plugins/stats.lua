f1486a795936ef0009c0dd557ea9a81c12910bdf
ashkanpj <ashkanpj.ap33@gmail.com>
2016-02-05 21:20:01 +0330

    a

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -138,11 +138,11 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[Ss](tats)$",
-    "^[Ss](tatslist)$",
-    "^[Ss](tats) (group) (%d+)",
-    "^[Ss](tats) (teleseed)",-- Put everything you like :)
-		"^[Tt](elefire)"-- Put everything you like :)
+    "^(stats)$",
+    "^(statslist)$",
+    "^(stats) (group) (%d+)",
+    "^(stats) (telefire)",-- Put everything you like :)
+		"^(telefire)"-- Put everything you like :)
     }, 
   run = run
 }
